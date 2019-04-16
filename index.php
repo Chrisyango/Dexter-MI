528d0623d25ada69e5584c8e351a9eeab87f14f8
Chris Yang <cyang287@yahoo.com>
2019-04-16 14:07:27 -0400

    finished html markup

---
+++ index.php
@@ -0,0 +1,362 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<title>Dexter, MI</title>
+	<meta name="description" content="">
+	<meta name="robots" content="index, follow">
+
+	<link rel="stylesheet" href="_assets_/plugins/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="_assets_/fonts/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="_assets_/css/animate.min.css">
+	<link rel="stylesheet" href="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.css">
+	<link rel="stylesheet" href="_assets_/css/layout.css">
+
+	<link rel="shortcut icon" href="_assets_/images/favicon.ico">
+	<link rel="apple-touch-icon" href="_assets_/images/touch-icon-iphone.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="_assets_/images/touch-icon-ipad.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="_assets_/images/touch-icon-iphone4.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="_assets_/images/touch-icon-ipad2.png">
+
+	<!--[if !IE]><!-->
+	<link rel="stylesheet" href="_assets_/plugins/add-to-homescreen/style/addtohomescreen.css">
+	<script src="_assets_/plugins/add-to-homescreen/src/addtohomescreen.min.js"></script>
+	<script>addToHomescreen();</script>
+	<!--<![endif]-->
+
+	<!-- Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body id="homepage">
+
+<header>
+	<a href="#main" id="skip" tabindex="0">SKIP TO CONTENT</a>
+
+	<div class="container">
+		<a href="./" id="logo" class="clearfix">
+			<img src="./_assets_/images/logo.png" alt="Logo">
+		</a><!-- /.logo -->
+
+		<div id="toggles" class="hidden-lg hidden-md">
+			<div tabindex="0" id="search-toggle-mobile" class="fa fa-search"></div>
+			<div tabindex="0" id="nav-toggle" class="fa fa-bars"></div>	
+		</div><!-- /#toggles -->
+
+		<div class="social-links hidden-sm hidden-xs fillLeft fillRight withPadding">
+			<a href="./" class="fa fa-facebook-square"></a>
+			<a href="./" class="fa fa-twitter"></a>
+			<a href="./" class="fa fa-instagram"></a>
+			<a href="./" class="fa fa-youtube"></a>
+			<span tabindex="0" id="search-toggle" class="fa fa-search"></span>
+		</div><!-- /.social-links -->
+
+		<nav>
+			<ul id="nav">
+				<li>
+					<a href="./">community</a>
+					<ul>
+						<li><a href="./">overview</a></li>
+						<li>
+							<a href="./">about dexter</a>
+							<ul>
+								<li><a href="./">demographics</a></li>
+								<li><a href="./">locations & maps</a></li>
+								<li><a href="./">history</a></li>
+							</ul>
+						</li>
+						<li>
+							<a href="./">farmer's market</a>
+							<ul>
+								<li><a href="./">dates + hours</a></li>
+								<li><a href="./">general information</a></li>
+							</ul>
+						</li>
+						<li>
+							<a href="./">community resources</a>
+							<ul>
+								<li><a href="./">commnuity garden</a></li>
+								<li><a href="./">education</a></li>
+								<li><a href="./">environmental information</a></li>
+								<li><a href="./">library</a></li>
+								<li><a href="./">local organizations</a></li>
+								<li><a href="./">parks & trails</a></li>
+								<li><a href="./">places of worship</a></li>
+								<li><a href="./">transportation</a></li>
+								<li><a href="./">tree information</a></li>
+							</ul>
+						</li>
+					</ul>
+				</li>
+				<li><a href="./">government</a></li>
+				<li><a href="./">services</a></li>
+				<li><a href="./">business</a></li>
+				<li><a href="./">i wanted to...</a></li>
+			</ul> <!-- /#nav -->
+		</nav>
+
+		<div id="search">
+			<form class="search-form" method="get" action="search.php">
+				<label class="fa fa-search" for="search-input"></label>
+				<input name="q" class="form-control search-input" placeholder="Enter search terms..." type="search" id="search-input">
+				<button><i class="fa fa-search"></i></button>
+			</form><!-- /.search-form -->
+		</div><!-- /#search -->
+	</div><!-- /.container -->
+</header><!-- /.clearfix -->
+
+<section id="slider">
+	<ul class="bxslider">
+		<li style="background: url('./_assets_/images/hero1.jpg') center no-repeat;background-size:cover;"></li>
+		<li style="background: url('./_assets_/images/hero2.jpg') center no-repeat;background-size:cover;"></li>
+		<li style="background: url('./_assets_/images/hero3.jpg') center no-repeat;background-size:cover;"></li>
+	</ul><!-- /.bxslider -->
+</section><!-- /#slider -->
+
+<main id="main">
+	<section id="useful-links">
+		<div class="container">
+			<div id="useful-links-wrapper" class="owl-carousel">
+				<a href="./" class="useful-link">
+					<div class="useful-link-icon" style="background:url('./_assets_/images/useful1.png') center no-repeat;background-size:cover;"></div>
+					<h3>Apply for Permits</h3>
+				</a><!-- /.useful-links -->
+				<a href="./" class="useful-link">
+					<div class="useful-link-icon" style="background:url('./_assets_/images/useful2.png') center no-repeat;background-size:cover;"></div>
+					<h3>Agendas + Minutes</h3>
+				</a><!-- /.useful-links -->
+				<a href="./" class="useful-link">
+					<div class="useful-link-icon" style="background:url('./_assets_/images/useful3.png') center no-repeat;background-size:cover;"></div>
+					<h3>Forms + Documents</h3>
+				</a><!-- /.useful-links -->
+				<a href="./" class="useful-link">
+					<div class="useful-link-icon" style="background:url('./_assets_/images/useful4.png') center no-repeat;background-size:cover;"></div>
+					<h3>City Maps + GIS</h3>
+				</a><!-- /.useful-links -->
+				<a href="./" class="useful-link">
+					<div class="useful-link-icon" style="background:url('./_assets_/images/useful5.png') center no-repeat;background-size:cover;"></div>
+					<h3>Online Payments</h3>
+				</a><!-- /.useful-links -->
+				<a href="./" class="useful-link">
+					<div class="useful-link-icon" style="background:url('./_assets_/images/useful6.png') center no-repeat;background-size:cover;"></div>
+					<h3>Contact Us</h3>
+				</a><!-- /.useful-links -->
+			</div><!-- /#useful-links-wrapper.owl-carousel -->
+		</div><!-- /.container -->
+	</section><!-- /#useful-links -->
+
+	<section id="content">
+		<section id="news-events-quick">
+			<div class="container">
+				<div class="row">
+					<section id="events" class="col-md-5">
+						<h2>events + meetings</h2>
+						<div id="calendar">
+							<img src="./_assets_/images/events.png" alt="Events">
+						</div><!-- /#calendar -->
+						<a href="./" class="btn">master calendar</a>
+					</section><!-- /#events -->
+
+					<section id="news" class="col-md-4">
+						<h2>news stories</h2>
+						<div id="news-links" class="owl-carousel">
+							<div class="news-link-wrapper">
+								<a href="./" class="news-link">
+									<div class="news-link-info">
+										<h3>local update: installation of rectangular rapid flashing beacon</h3>
+										<p>
+											As part of the Grandview Commons condominium project, a Rectangular Rapid Flashing Beacon (RRFB) has been installed at the intersection of Baker and Grand. The RRFB can be activated by... 
+										</p>
+										<span>december 14, 2018</span>
+										<a href="./">read more</a>
+									</div><!-- /.news-link-info -->
+								</a><!-- /.news-link -->
+								<a href="./" class="news-link">
+									<div class="news-link-info">
+										<h3>leaf collection information</h3>
+										<p>
+											The last day for loose leaf collection via vacuum truck is Friday, December 14, 2018. If you have a pile to be collected, please send an e-mail to cnicholls@dextermi.gov for more information... 
+										</p>
+										<span>december 13, 2018</span>
+										<a href="./">read more</a>
+									</div><!-- /.news-link-info -->
+								</a><!-- /.news-link -->
+								<a href="./" class="news-link">
+									<div class="news-link-info">
+										<h3>ice rink installation</h3>
+										<p>
+											The ice rink in Monument Park has been installed. Opening day will be announced on the website, e-mail update and Facebook page...
+										</p>
+										<span>december 13, 2018</span>
+										<a href="./">read more</a>
+									</div><!-- /.news-link-info -->
+								</a><!-- /.news-link -->
+							</div><!-- /.news-links-wrapper -->
+						</div><!-- /#news-links -->
+						<a href="./" class="btn">news archives</a>
+					</section><!-- /#news -->
+
+					<section id="quick-links" class="col-md-3">
+						<h2>quick links</h2>
+						<ul>
+							<li><a href="./">Bid Opportunities</a></li>
+							<li><a href="./">Find + Apply for Permits</a></li>
+							<li><a href="./">City Charter</a></li>
+							<li><a href="./">City Newsletter</a></li>
+							<li><a href="./">Emergency Services</a></li>
+							<li><a href="./">Farmer's Market</a></li>
+							<li><a href="./">Nayor's Report</a></li>
+							<li><a href="./">Ordinances</a></li>
+							<li><a href="./">Zoning + Planning</a></li>
+							<li><a href="./">Residential Handbook</a></li>
+							<li><a href="./">Billing + Payments</a></li>
+							<li><a href="./">FAQ + Information</a></li>
+						</ul>
+					</section><!-- /#local-business -->
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+		</section><!-- /#news-events -->
+
+		<section id="welcome">
+			<div id="welcome-img" style="background:url('./_assets_/images/welcome.jpg') center no-repeat;background-size:cover;"></div>
+			<div id="welcome-info">
+				<h1><span>welcome to the city of</span>dexter<span>michigan</span></h1>
+				<p>
+					The City of Dexter is located in Southeast Michicgan, along the banks of the Huron River and Mill Creek. Residents and visitors can enjoy a day on the river, walking along nature trails, or biking on the County Border-to-Border trail, stopping in our historic downtown for lunch and some shopping. Cultural activites inlcude the loacl theatre, historical mueseum, summer concerts, ice skating in the winter, festivals, and simply playing the parks.
+				</p>
+				<a href="./">learn more</a>
+			</div><!-- /#welcome-info -->
+		</section><!-- /#welcome -->
+	</section><!-- /#content -->
+</main>
+
+<footer>
+	<div class="container">
+		<section id="top-footer">
+			<div id="left-footer">
+				<span>&copy; City of Dexter, MI</span>
+				<span>(P) 734-426-8303</span>
+				<span>(F) 734-426-5614</span>
+				<span>Address: 8123 Main Street, 2nd Floor, Dexter, MI 48130</span>
+				<span><span>Hours:</span> M-F 9:00 AM-5:00 PM</span>
+			</div><!-- /#left-footer -->
+		
+			<div id="right-footer">
+				<div id="top-right">
+					<div id="google-translate">
+						<script src="https://code.jquery.com/jquery-2.1.4.js"></script>
+						<div id="google_translate_element"></div>
+						<script type="text/javascript">
+							function googleTranslateElementInit() {
+								new google.translate.TranslateElement({  
+									pageLanguage: 'en', 
+									layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+								}, 'google_translate_element');
+							}
+						</script>
+						<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+					</div><!--/#google-translate.control.hidden-sm.hidden-xs-->
+					<a href="./">login</a>
+					<div class="social-links">
+						<a href="./" class="fa fa-share-alt"></a>
+						<a href="./" class="fa fa-facebook"></a>
+					</div><!-- /.social-links -->
+				</div><!-- /#top-right -->
+			
+				<div id="bottom-right">
+					<a href="./">staff directory</a>
+					<a href="./">council directory</a>
+				</div><!-- /#bottom-right -->
+			</div><!-- /#right-footer -->
+		</section><!-- /#top-footer -->
+
+		<section id="revize-logo">
+			<span>Powered by <a href="https://www.revize.com/">revize</a>, The Government Website Experts.</span>
+		</section><!-- /#revize-logo -->
+	</div><!-- /.container -->
+</footer>
+
+<div class="alert alert-dismissible floating-alert fade in os-animation" data-os-animation="fadeInUpBig" data-os-animation-delay="2s" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	<h4>Alert Title 30 characters Max</h4>
+	<p>Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120</p>
+	<p><a href="#" class="btn">Read More</a></p>
+</div><!-- /.alert -->
+
+
+<!-- Share widget make into an include file -->
+<button type="button" class="share-btn floating-share-btn" data-toggle="modal" data-target="#shareModal">
+	<i class="fa fa-share-alt"></i>
+	<span>Share</span>
+</button>
+
+<div class="modal fade" id="shareModal" tabindex="-1" role="dialog" aria-labelledby="shareModal">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Share this page</h4>
+			</div>
+			<div class="modal-body">
+				<div class="copylink">
+					<p>Copy and paste this code into your website.</p>
+					<pre>&lt;a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>">Your Link Name&lt;/a&gt;</pre>
+				</div><!-- /.copylink -->
+				<div class="share-btns">
+					<p>Share this page on your favorite Social network</p>
+					<div class="row">
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-facebook" onclick="return !window.open(this.href, 'facebook ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-facebook"></i> Facebook
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.twitter.com/home?status=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-twitter" onclick="return !window.open(this.href, 'twitter ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-twitter"></i> Twitter
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.plus.google.com/share?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-google" onclick="return !window.open(this.href, 'google ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-google-plus"></i> Google Plus
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.reddit.com/submit?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-reddit" onclick="return !window.open(this.href, 'redit ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-reddit"></i> Reddit
+							</a>
+						</div>
+					</div><!-- /.row -->
+				</div><!-- /.share-btns -->
+				<button type="button" class="btn btn-success btn-lg" data-dismiss="modal">Close</button>
+			</div><!-- /.modal-body -->
+		</div>
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- Share widget make into an include file -->
+
+
+<script src="_assets_/js/jquery.min.js"></script>
+<script src="_assets_/plugins/modernizr/modernizr.custom.js"></script>
+<script src="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.min.js"></script>
+<script src="_assets_/plugins/jquery.bxslider/jquery.bxslider.min.js"></script>
+<script src="_assets_/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="_assets_/plugins/twitter/js/jquery.tweet.min.js"></script>
+<script src="_assets_/plugins/instafeed/instafeed.min.js"></script>
+<script src="_assets_/plugins/sticky/jquery.sticky.js"></script>
+<script src="_assets_/plugins/matchHeight/dist/jquery.matchHeight-min.js"></script>
+<script src="_assets_/plugins/revizeWeather/js/revizeWeather.min.js"></script>
+<script src="_assets_/plugins/waypoints/waypoints.min.js"></script>
+<script src="_assets_/js/scripts.js"></script>
+
+</body>
+</html>
